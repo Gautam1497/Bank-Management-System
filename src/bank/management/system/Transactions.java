ed7edd83f4d5ed1e336751c36e4495c050c593ec
gautamsingh1497 <gk1497singh@gmail.com>
2024-08-27 22:41:34 +0530

    Page 5: Transactions

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -85,6 +85,14 @@ public class Transactions extends JFrame implements ActionListener {
         } else if (e.getSource()==fastcash) {
             setVisible(false);
             new FastCash(pinnumber).setVisible(true);
+        }else if (e.getSource()==pinchange) {
+            setVisible(false);
+            new PinChange(pinnumber).setVisible(true);
+        }else if (e.getSource()==balanceenquiry) {
+            setVisible(false);
+            new BalanceEnquiry(pinnumber).setVisible(true);
+        }else if (e.getSource()==ministatement) {
+            new MiniStatement(pinnumber).setVisible(true);
         }
     }
 }
