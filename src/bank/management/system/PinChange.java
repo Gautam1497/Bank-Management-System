0e21ae290992773cedfda1fcbfbd644991a1904e
gautamsingh1497 <gk1497singh@gmail.com>
2024-08-27 21:21:17 +0530

    Page 10: Pin Change

--- src/bank/management/system/PinChange.java
+++ src/bank/management/system/PinChange.java
@@ -8,10 +8,15 @@ import java.awt.event.ActionListener;
 public class PinChange extends JFrame implements ActionListener {
 
     JLabel text,pintext,repintext;
-    JTextField pin,repin;
+    JPasswordField pin,repin;
     JButton change,back;
+    String pinnumber;
 
-    PinChange(String pinchange){
+    PinChange(String pinnumber){
+
+        this.pinnumber=pinnumber;
+        setLayout(null);
+
         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
         Image i2=i1.getImage().getScaledInstance(900,900, Image.SCALE_DEFAULT);
         ImageIcon i3=new ImageIcon(i2);
@@ -31,7 +36,7 @@ public class PinChange extends JFrame implements ActionListener {
         pintext.setForeground(Color.WHITE);
         image.add(pintext);
 
-        pin=new JTextField();
+        pin=new JPasswordField();
         pin.setFont(new Font("Raleway",Font.BOLD,25));
         pin.setBounds(330,320,180,30);
         image.add(pin);
@@ -42,7 +47,7 @@ public class PinChange extends JFrame implements ActionListener {
         repintext.setForeground(Color.WHITE);
         image.add(repintext);
 
-        repin=new JTextField();
+        repin=new JPasswordField();
         repin.setFont(new Font("Raleway",Font.BOLD,25));
         repin.setBounds(330,360,180,30);
         image.add(repin);
@@ -59,7 +64,7 @@ public class PinChange extends JFrame implements ActionListener {
 
         setSize(900,900);
         setLocation(300,0);
-//        setUndecorated(true);
+        setUndecorated(true);
         setVisible(true);
     }
 
@@ -68,7 +73,42 @@ public class PinChange extends JFrame implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource()==change){
+            try {
+                String npin = pin.getText();
+                String rpin = repin.getText();
 
+                if (!npin.equals(rpin)) {
+                    JOptionPane.showMessageDialog(null, "Entered PIN does not match");
+                    return;
+                }if(npin.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Please Enter PIN");
+                    return;
+                }if(rpin.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Please Enter new PIN");
+                    return;
+                }
+
+                Conn conn=new Conn();
+                String query1="update bank set pin ='"+rpin+"' where pin = '"+pinnumber+"'";
+                String query2="update login set pin ='"+rpin+"' where pin = '"+pinnumber+"'";
+                String query3="update signupthree set pinnumber ='"+rpin+"' where pinnumber = '"+pinnumber+"'";
+                conn.s.executeUpdate(query1);
+                conn.s.executeUpdate(query2);
+                conn.s.executeUpdate(query3);
+
+                JOptionPane.showMessageDialog(null, "PIN changed successfully");
+
+                setVisible(false);
+                new Transactions(rpin).setVisible(true);
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }else {
+            setVisible(false);
+            new Transactions(pinnumber).setVisible(true);
+        }
     }
 }
